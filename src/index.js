5be24ea45c2541584f768da73b58a6be8732fe89
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-08-18 18:34:20 -0500

    Ya se traen los datos de la api y se renderizan.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
-console.log("I'm alive");
+const BASE_URL = "https://rickandmortyapi.com/api";
 
-const BASE_URL = "https://www.fakeurl.com";
+function create(element) {
+  return document.createElement(element);
+}
 
 async function main() {
   const nodes = [];
   const container = document.getElementById("app");
-  const response = await window.fetch(BASE_URL + "/route");
-  const data = await response.json();
-  data.forEach((item) => {
-    console.log(item);
-  });
+  try {
+    const response = await window.fetch(BASE_URL + "/character", {
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await response.json();
+    data.results.forEach((character) => {
+      const content = create("div");
+      content.className = "character";
+      const { name, status, species, gender, image } = character;
+      const img = create("img");
+      img.src = image;
+      const title = create("p");
+      title.textContent = name;
+      const detailsContent = create("div");
+      detailsContent.className = "details";
+      const statusP = create("p");
+      statusP.textContent = status;
+      const speciesP = create("p");
+      speciesP.textContent = species;
+      const genderP = create("p");
+      genderP.textContent = gender;
+      detailsContent.append(statusP, speciesP, genderP);
+      content.append(img, title, detailsContent);
+      nodes.push(content);
+    });
+    container.append(...nodes);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 main();
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  entry: "./src/index.js",
   output: {
     filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
+  mode: "development",
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
