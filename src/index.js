73830b979011327fd925fd80ec618fddf716c53c
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-08-18 14:18:23 -0500

    Cree la funcion main en el index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log("I'm alive");
+
+const BASE_URL = "https://www.fakeurl.com";
+
+async function main() {
+  const nodes = [];
+  const container = document.getElementById("app");
+  const response = await window.fetch(BASE_URL + "/route");
+  const data = await response.json();
+  data.forEach((item) => {
+    console.log(item);
+  });
+}
+
+main();
